597e18f68b7375e2d281f96260ec8f391543ca85
iwasaki-mode <iwasaki@mode-co.jp>
2022-04-16 20:15:14 +0900

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         create: (_) => MainModel(),
         child: Scaffold(
           appBar: AppBar(
-            title: Text('アルタイル'),
+            title: Text('センチュリーホテル'),
           ),
           body: Consumer<MainModel>(
             builder: (context, model, child) {
